beb3b409696cdbe843a7611a41f5a2c54b482776
Khang <khang.nguyenduyvn@gmail.com>
2024-03-24 12:19:27 +0700

    fix: disable ogImage for deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,4 +14,7 @@ export default defineNuxtConfig({
     name: "Thu Nhập Online",
     description: "Khám phá các sản phẩm online thành công, mang lại thu nhập",
   },
+  ogImage: {
+    enabled: false,
+  },
 });
