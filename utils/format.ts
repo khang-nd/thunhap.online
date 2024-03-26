0c40da44682838c3a069787c66f219047abd8fb1
Khang <khang.nguyenduyvn@gmail.com>
2024-03-26 12:25:08 +0700

    fix: tweak price formatting

--- utils/format.ts
+++ utils/format.ts
@@ -12,8 +12,17 @@ export function formatPrice(
   const prefix = options?.prefix || (locale === "vi" ? "" : "$");
   const suffix = options?.suffix || (locale === "vi" ? "đ" : "");
   const thousand = options?.thousand || ",";
-  const replacedValue = value
-    .toString()
-    .replace(/\B(?=(\d{3})+(?!\d))/g, thousand);
-  return prefix + replacedValue + suffix;
+  const valueString = value.toString();
+  let result = "";
+
+  if (valueString.length > 9) {
+    result = valueString.slice(0, -9) + "B";
+  } else if (valueString.length > 6) {
+    result = valueString.slice(0, -6) + "M";
+  } else if (valueString.length > 5) {
+    result = valueString.slice(0, -3) + "K";
+  } else {
+    result = valueString.replace(/\B(?=(\d{3})+(?!\d))/g, thousand);
+  }
+  return prefix + result + suffix;
 }
