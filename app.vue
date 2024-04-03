af33cd32c8d8c0236a8787a134057c3d093226eb
Khang <khang.nguyenduyvn@gmail.com>
2024-04-03 22:40:20 +0700

    feat: i18n meta tags

--- app.vue
+++ app.vue
@@ -9,9 +9,15 @@
 <script setup lang="ts">
 import { ConfigProvider } from 'radix-vue';
 
+const i18nHeader = useLocaleHead({
+  identifierAttribute: 'id',
+  addSeoAttributes: true,
+})
+
 useHead({
   titleTemplate: (title) => title ? `${title} - Thu Nhập Online` : "Thu Nhập Online",
   meta: [
+    ...i18nHeader.value.meta.map((meta: any) => (meta)),
     { property: 'og:image', content: 'https://cdn.thunhap.online/og-image.jpg' }
   ]
 })
